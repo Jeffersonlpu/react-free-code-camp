92a1d8419ca6a2087c13b50bc824cdd7072f4e83
Jefferson Silva <111249250+Jeffersonlpu@users.noreply.github.com>
2023-01-24 12:46:16 -0300

    13-props-practice

--- airbnb-clone/12-destructuring-props/Contact.js
+++ airbnb-clone/12-destructuring-props/Contact.js
@@ -1,35 +1,29 @@
 import React from "react"
-import Contact from "./Contact"
 
-function App() {
+// Challenge: fix the bug, now that we've destructured the props object
+export default function Contact(props) {
     return (
-        <div className="contacts">
-            <Contact 
-                img="./images/mr-whiskerson.png" 
-                name="Mr. Whiskerson"
-                phone="(212) 555-1234"
-                email="mr.whiskaz@catnap.meow"
-            />
-            <Contact 
-                img="./images/fluffykins.png"
-                name="Fluffykins"
-                phone="(212) 555-2345"
-                email="fluff@me.com"
-            />
-            <Contact 
-                img="./images/felix.png"
-                name="Felix"
-                phone="(212) 555-4567"
-                email="thecat@hotmail.com"
-            />
-            <Contact 
-                img="./images/pumpkin.png"
-                name="Pumpkin"
-                phone="(0800) CAT KING"
-                email="pumpkin@scrimba.com"
-            />
+        <div className="contact-card">
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{props.email}</p>
+            </div>
         </div>
     )
 }
 
-export default App
+// const person = {
+//     img: "./images/mr-whiskerson.png",
+//     name: "Mr. Whiskerson",
+//     phone: "(800) 555-1234",
+//     email: "mr.whiskaz@catnap.meow"
+// }
+
+// const {img, name} = person
+// console.log(name)
